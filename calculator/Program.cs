42d404edd569c1ecc393a992e88b8eed23465a5a
guree <guree@DESKTOP-HOL6NIT>
2024-11-08 00:05:03 +0100

    Добавьте файлы проекта.

---
+++ calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Clear(); 
+                Write($"первое число: ");
+                double.TryParse(ReadLine(), out double num1);
+                Write($"действие: ");
+                string todo = ReadLine();
+                Write($"второе число: ");
+                double.TryParse(ReadLine(), out double num2);
+                if (todo == "/" && num2 == 0) { Write("ошибка\n"); }
+                else if (todo == "+") { Write($"решение: {num1 + num2} \n"); }
+                else if (todo == "-") { Write($"решение: {num1 - num2} \n"); }
+                else if (todo == "*") { Write($"решение: {num1 * num2}\n"); }
+                else if (todo == "/" && num2 != 0) { Write($"решение: {num1 / num2}\n"); }
+                else if (todo == "%") { Write($"решение: {num1 % num2}\n"); }
+                else if (todo == "^") { Write($"решение: {Pow(num1, num2)}\n"); }
+                else { Write("ошибка\n"); }
+                Write("посчитать что то ещё раз? 1 - да все остальное - нет\n");
+                if (ReadLine() != "1") { break; }
+            }
+        }
+    }
+}
